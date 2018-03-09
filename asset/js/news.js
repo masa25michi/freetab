9eface4547375873f7f2c309331d47fbdd217bfa
Masamichi Tanaka <masa25michi@hotmail.com>
2018-03-09 22:32:12 +0800

    add quote feature

--- asset/js/news.js
+++ asset/js/news.js
@@ -3,7 +3,6 @@ $(function(){
         type: 'GET',
         url: 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=b7df51ebe1ff4cceb37cae441f754fb3',
         success: function (data) {
-
             $.each(data['articles'], function(index, element) {
                 $('.news-list').append(newsitem(element['title'],element['description'], element['url']));
             });
---
+++ asset/js/quote.js
@@ -0,0 +1,7 @@
+$(function(){
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
+        $("#quote-text").html('<div style="font-style: italic;">'+a[0].content + "<p style='padding:0px;margin:0px'>— " + a[0].title + "</p></div>")
+        $("#quote-text").fadeIn()
+    });
+});
+
